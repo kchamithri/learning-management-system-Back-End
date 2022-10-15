b5cc95f8efd2ef310c22dcd419492fef0921088c
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2022-10-15 15:56:44 +0530

    role repo added

---
+++ src/main/java/com/iad/lmsBackend/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.iad.lmsBackend.repository;
+
+import java.util.Optional;
+
+import com.iad.lmsBackend.entity.ERole;
+import com.iad.lmsBackend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+
+}
--- src/main/java/com/iad/lmsBackend/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iad.lmsBackend.repository;
-
-import com.iad.lmsBackend.entity.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, String> {
-}

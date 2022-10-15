26ca05a4d8a671e52a9baec02889d6ac3f1a9882
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2022-10-15 15:56:55 +0530

    user repo added

---
+++ src/main/java/com/iad/lmsBackend/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.iad.lmsBackend.repository;
+
+import java.util.Optional;
+
+import com.iad.lmsBackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}

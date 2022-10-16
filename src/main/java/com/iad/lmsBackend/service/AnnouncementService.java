f7720426dd2519bdb4b48dd02569868fe3c65af8
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2022-10-16 11:53:35 +0530

    announcememnt apis created

--- src/main/java/com/iad/lmsBackend/service/AnnouncementService.java
+++ src/main/java/com/iad/lmsBackend/service/AnnouncementService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AnnouncementService {
@@ -17,10 +18,54 @@ public class AnnouncementService {
 
     public AnnouncementResponse viewAnnouncements(AnnouncementRequest request){
         AnnouncementResponse response = new AnnouncementResponse();
-        List<Announcement> announcementList = announcementRepository.viewAll(request);
+        try{
+            List<Announcement> announcementList = announcementRepository.findByCourseId(request.getCourseId());
+            response.setMessage("successfully retrieved");
+            response.setStatus("00");
+            response.setAnnouncementList(announcementList);
+        }catch (Exception e){
+            System.out.println(e);
+        }
 
-        response.setMessage("successfully retrieved");
-        response.setStatus("00");
+        return response;
+    }
+
+    public AnnouncementResponse saveAnnouncements (AnnouncementRequest request){
+        AnnouncementResponse announcementresponce = new AnnouncementResponse();
+        try {
+            Announcement announcement = new Announcement(request.getAnnouncementId(), request.getAnnouncementName(), request.getDescription(), request.getCourseId());
+            announcementRepository.save(announcement);
+            announcementresponce.setStatus("00");
+            announcementresponce.setMessage("Successfully Saved");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        return announcementresponce;
+    }
+
+    public AnnouncementResponse updateAnnouncements(AnnouncementRequest request){
+        AnnouncementResponse response = new AnnouncementResponse();
+        try{
+            Announcement announcement = new Announcement(request.getAnnouncementId(), request.getAnnouncementName(),request.getDescription(), request.getCourseId());
+            announcementRepository.save(announcement);
+            response.setStatus("00");
+            response.setMessage("successfully updated");
+        }catch(Exception e){
+            System.out.println("Exception in announcement update");
+        }
+        return response;
+    }
+
+    public AnnouncementResponse deleteAnnouncements(AnnouncementRequest request){
+        AnnouncementResponse response = new AnnouncementResponse();
+        try{
+            announcementRepository.deleteById(String.valueOf(request.getAnnouncementId()));
+            response.setStatus("00");
+            response.setMessage("successfully deleted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
         return response;
     }
 }
